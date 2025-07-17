5c066f08125a4b63a0d81fa8c24c64990bcbb77a
ziziduck <tsunglian@gmail.com>
2025-07-17 13:51:39 +0800

    使用 watch

--- VUE_LABS/lab13_vite_composition2/src/components/MyComp1.vue
+++ VUE_LABS/lab13_vite_composition2/src/components/MyComp1.vue
@@ -8,6 +8,11 @@
         <hr />
         <input placeholder="courseId" v-model="courseId" />
         <input placeholder="courseFullName" v-model="courseFullName" />
+        <hr />
+        <input type="text"
+        placeholder="alternative course full name"
+        ref="courseFullNameInput">
+        <button @click="setCourseFullNameInput">set course full name</button>
     </div>
 </template>
 
@@ -18,6 +23,7 @@ export default {
         const price = ref(24000)
         const courseId = ref("")
         const courseFullName = ref("")
+        const courseFullNameInput = ref()
         const courseDisplayName = computed(function () {
             // console.log(".")
             return courseId.value + '../..' + courseFullName.value
@@ -27,9 +33,18 @@ export default {
             course.duration = course.duration + 7
             price.value += 5000
         }
-        watch(price, function (newValue, oldValue) {
-            console.log(`price goes from ${oldValue} to ${newValue}`)
+        // watch(price, function (newValue, oldValue) {
+        //     console.log(`price goes from ${oldValue} to ${newValue}`)
+        // })
+        watch([price, courseId], function (newValue, oldValue) {
+            //console.log(newValue)
+            //console.log(oldValue)
+            console.log(`price goes from ${oldValue[0]} to ${newValue[0]}`)
+            console.log(`name goes from ${oldValue[1]} to ${newValue[1]}`)
         })
+        function setCourseFullNameInput() {
+            courseFullName.value = courseFullNameInput.value.value
+        }
         // function setCourseId(e) {
         //     courseId.value = e.target.value
         // }
@@ -38,7 +53,8 @@ export default {
         // }
         return {
             course, extraDuration, price, courseDisplayName,
-            courseId, courseFullName
+            courseId, courseFullName,courseFullNameInput,
+            setCourseFullNameInput
         }
     }
 }
